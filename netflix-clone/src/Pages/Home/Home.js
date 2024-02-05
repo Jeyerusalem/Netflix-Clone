e6258737f33c267cf9a618e1946360694996a409
Jeyerusalem <111075724+Jeyerusalem@users.noreply.github.com>
2024-02-05 23:41:54 +0300

    row

--- netflix-clone/src/Pages/Home/Home.js
+++ netflix-clone/src/Pages/Home/Home.js
@@ -2,13 +2,15 @@ import React from "react";
 import Header from "../../Components/Header/Header";
 
 import Footer from "../../Components/Footer/Footer";
-import Banner from "../../Components/Banner/Bannerr"
+import Banner from "../../Components/Banner/Bannerr";
+import RowList from "../../Components/Rows/RowList/RowList";
 
 const Home = () => {
   return (
     <div>
       <Header />
-   < Banner/>
+      <Banner />
+      <RowList />
       <Footer />
     </div>
   );
